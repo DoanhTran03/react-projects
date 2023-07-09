62ca4ff0166b44281c73cc204f54f19279b5a934
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-08 23:20:47 -0400

    Build BirthdayContainer, BirthdayList, Birthday Items components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import "./index.css"
+import BirthdayContainer from './components/BirthdayContainer'
+import BirthdayItem from "./components/BirthdayItem"
+import BirthdayList from "./components/BirthdayList"
 
 function App() {
 
   return (
     <>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, pariatur!s</h1>
-    <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, sunt?</h3>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius, quos.</p>
+      <BirthdayContainer></BirthdayContainer>
+      
     </>
   )
 }
