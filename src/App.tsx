a2d1cfde6992a47d3ed18f73bb6e16f961d51fb4
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-10 23:39:54 -0400

    Add Typography to CSS stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-
+import "./index.css"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, suscipit.</h1>
+      <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, libero.</h3>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates, a?</p>
     </>
   )
 }
