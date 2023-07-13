f11c3526d33bb19809c72e0fcf5e53796c4a2c0f
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-13 10:09:52 -0400

    Build MenuGrid component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react"
 import FoodItem from "./components/FoodItem"
 import CategoryTab from "./components/CategoryTab"
+import MenuGrid from "./components/MenuGrid"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <CategoryTab></CategoryTab>
+    <MenuGrid></MenuGrid>
     </>
   )
 }
---
+++ src/components/MenuGrid.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FoodItem from './FoodItem'
+
+const MenuGrid = () => {
+  return (
+    <div className='menuGrid'>
+        <FoodItem></FoodItem>
+        <FoodItem></FoodItem>
+        <FoodItem></FoodItem>
+    </div>
+  )
+}
+
+export default MenuGrid
