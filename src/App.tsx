5d6b0a7caad09361bf72c96f783f418c906acd3d
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-13 10:03:19 -0400

    Build CategoryTab component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from "react"
 import FoodItem from "./components/FoodItem"
+import CategoryTab from "./components/CategoryTab"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <FoodItem></FoodItem>
+    <CategoryTab></CategoryTab>
     </>
   )
 }
---
+++ src/components/CategoryTab.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CategoryTab = () => {
+  return (
+    <div className="categoryTab">
+      <button className="categoryTab__btn">All</button>
+      <button className="categoryTab__btn">Food</button>
+      <button className="categoryTab__btn">Drink</button>
+      <button className="categoryTab__btn">Average</button>
+    </div>
+  );
+};
+
+export default CategoryTab;
