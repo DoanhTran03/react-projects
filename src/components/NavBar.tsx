2791134bb67b1571e598e5e2c53d173f4dbafaba
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-21 03:29:56 -0400

    Create global context and add [link, setLink] as value

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,11 @@
 import {PiBooksLight} from "react-icons/pi"
 import {AiOutlineShoppingCart} from "react-icons/ai"
+import { useGlobalState } from "../contexts/context";
 const NavBar = () => {
+    const context = useGlobalState();
+    const hoverHandle = (event: React.MouseEvent) => {
+        context?.setLink(event.currentTarget);
+    }  
   return (
     <div className="navBar">
       <div className="navBar__heading">
@@ -8,13 +13,13 @@ const NavBar = () => {
         <h1>Ademo Books</h1>
       </div>
       <ul className="navBar__links">
-        <li>
+        <li onMouseOver={(event) => hoverHandle(event)}>
           <a href="">our project</a>
         </li>
-        <li>
+        <li onMouseOver={(event) => hoverHandle(event)}>
           <a href="">about us</a>
         </li>
-        <li>
+        <li onMouseOver={(event) => hoverHandle(event)}>
           <a href="">contact</a>
         </li>
       </ul>
