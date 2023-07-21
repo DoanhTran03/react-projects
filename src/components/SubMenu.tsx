d51fa82a62dea4e52ff29972097d6194d14ba71c
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-21 10:24:37 -0400

    Add Triangle pointer on top of SubMenu component

--- src/components/SubMenu.tsx
+++ src/components/SubMenu.tsx
@@ -6,8 +6,8 @@ const SubMenu = () => {
     /*!--Change location according to the link*/
     let subMenuRef = useRef<HTMLDivElement>(null);
     const linkRect = context?.link?.getBoundingClientRect();
-    const center = linkRect ? (linkRect?.left + linkRect?.right) / 2 : "";
-    const bottom = linkRect ? (linkRect.bottom + 10) : "";
+    const center = linkRect ? ((linkRect?.left + linkRect?.right) / 2)-20 : "";
+    const bottom = linkRect ? (linkRect.bottom + 15) : "";
 
     if (subMenuRef !== null) {
         if (subMenuRef.current !== null) {subMenuRef.current.style.left = `${center}px`;
