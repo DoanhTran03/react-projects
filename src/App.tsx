33eafe70485ff2f8d8e4cb68ab21244cb353299b
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-21 00:31:06 -0400

    Add Typography and Global Style in css stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, incidunt!
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, sed.</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
     </>
   )
 }
