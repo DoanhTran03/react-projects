ac21e6efa46593596495ea2a97396b4a20587c6a
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-27 11:30:10 -0400

    Build InfoCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import InfoCard from './components/InfoCard'
 
 function App() {
   
 
   return (
     <>
-      
+      <InfoCard></InfoCard>
     </>
   )
 }
--- src/components/InfoCard.tsx
+++ src/components/InfoCard.tsx
@@ -1,1 +1,19 @@
-/**/
+interface Props {
+    id: number,
+    title: string,
+    date: any,
+    length: number,
+    snippet: string
+}
+
+const InfoCard = () => {
+  return (
+    <div className='infoCard'>
+        <h3>Lorem ipsum dolor sit amet.</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, non!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi, facere?</p>
+    </div>
+  )
+}
+
+export default InfoCard
