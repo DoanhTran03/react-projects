82bc1c833b3d5f234d804632670efa7d52796257
DoanhTran03 <doanhtran2003@gmail.com>
2023-07-27 12:51:33 -0400

    Add changing color mode in DarkMode component

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import comments from "../data/data"
 import InfoCard from './InfoCard'
 
 const DarkMode = () => {
+    const [theme, setTheme] = useState("light-theme");
+    useEffect(()=>{
+        document.documentElement.className = theme;
+    },[theme])
   return (
     <div className='darkMode'>
         <div className='darkMode__heading'>
             <h1>Dark Mode Project</h1>
-            <button>Toggle</button>
+            <button onClick={() => {theme == "light-theme" ? setTheme("dark-theme") : setTheme("light-theme")}}>Toggle</button>
         </div>
         <div className="darkMode__content">
             {comments.map(comment => <InfoCard key={comment.id} id={comment.id} title={comment.title} date={comment.date} length={comment.length} snippet={comment.snippet}></InfoCard>)}
